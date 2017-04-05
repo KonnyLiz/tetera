49b46671164c0b81b10b8e89b3eb2d3f3bb77c55
Konny Amaya <konny@Kuro.lan>
2017-04-04 20:04:44 -0600

    subida desde Kuro

---
+++ build/CMakeFiles/tetera.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tetera.dir/main.cpp.o"
+  "tetera.pdb"
+  "tetera"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tetera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
