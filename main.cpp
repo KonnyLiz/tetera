e234d14fcf98520f90951328755c64779c6c811d
KonnyLiz <pinchehugochan@gmail.com>
2017-04-18 08:42:28 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,131 +1,113 @@
-#include <GL/glu.h>
+#include <GL/gl.h>
 #include <GL/glut.h>
+#include <stdlib.h>
 
-double rotate_y=0;
-double rotate_x=0;
-double rotate_z=0;
+void init(void)
+{
+// Ubicamos la fuente de luz en el punto
+   GLfloat light_position[] = { -10.0, 1.0, 1.0, 0.0 };
+ //GLfloat luz_ambiental[] = { 0.2, 0.2, 0.2, 1.0 };
 
-GLfloat X = 0.0f;
-GLfloat Y = 0.0f;
-GLfloat Z = 0.0f;
-GLfloat scale = 1.0f;
-void init(void){
-    /* selecciona el color de borrado */
-    glClearColor (0.0, 0.0, 0.0, 0.0);
-    glShadeModel (GL_FLAT);
+
+// Activamos la fuente de luz
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glDepthFunc(GL_LESS);
+    glEnable(GL_DEPTH_TEST);
+
+glLightfv(GL_LIGHT0,GL_POSITION,light_position);
+// Queremos que se dibujen las caras frontales
+// y con un color solido de relleno.
+  glPolygonMode(GL_FRONT, GL_FILL);
+
 }
+
+void reshape(int w, int h)
+{
+    glViewport(0, 0,  (GLsizei) w, (GLsizei) h);
+// Activamos la matriz de proyeccion.
+    glMatrixMode(GL_PROJECTION);
+// "limpiamos" esta con la matriz identidad.
+    glLoadIdentity();
+// Usamos proyeccion ortogonal
+    glOrtho(-300, 300, -300, 300, -300, 300);
+// Activamos la matriz de modelado/visionado.
+    glMatrixMode(GL_MODELVIEW);
+// "Limpiamos" la matriz
+    glLoadIdentity();
+}
+
+// Aqui ponemos lo que queremos dibujar.
 void display(void)
 {
-    /* borra la pantalla */
-    glClear (GL_COLOR_BUFFER_BIT);
-     glLoadIdentity();
-    // Rotar en el eje X,Y y Z
-    glRotatef( rotate_x, 1.0, 0.0, 0.0 );
-    glRotatef( rotate_y, 0.0, 1.0, 0.0 );
-    glRotatef( rotate_z, 0.0, 0.0, 1.0 );
-    glTranslatef(X, Y, Z); 	// Transladar en los 3 ejes
-    glScalef(scale, scale, scale);
-    /* seleccionamos la matriz modelo/vista */
-    glMatrixMode(GL_MODELVIEW);
-    
-    /* color */
-    glColor3f(1.0, 1.0, 1.0);
-   /* transformación modelo/vista */
-    gluLookAt(0.0,0.0,0.2, //punto de vista en el eje X,Y,Z //desde arriba 0.0,0.1,0.2  abajo 0.0,-1,0.2
-              0.0,0.0,0.0, //Centro del Objeto en el eje X,Y,Z
-              0.0,1.0,0.0); //Orientación de la cámara (vector UP el eje X,Y,Z)
-    /* Dibujamos una tetera */
-    glutWireTeapot(1.0);
-    /* Vacia el buffer de dibujo */
-    glFlush ();
+// Propiedades del material
+
+    GLfloat mat_ambient[] = { 0.19225f, 0.19225f, 0.19225f,1.0f };
+    GLfloat mat_diffuse[] = { 0.50754f, 0.50754f, 0.50754f, 1.0f };
+    GLfloat mat_specular[] = { 0.508273f, 0.508273f, 0.508273f, 1.0f };
+    GLfloat shine[] = {27.8974f};
+
+// "Limpiamos" el frame buffer con el color de "Clear", en este
+// caso negro.
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glMatrixMode( GL_MODELVIEW_MATRIX );
+    glLoadIdentity();
+
+// Rotacion de 20 grados en torno al eje x
+    glRotated(25.0, 1.0, 0.0, 0.0);
+// Rotacion de -30 grados en torno al eje y
+    glRotated(-30.0, 0.0, 1.0, 0.0);
+// Dibujamos una "Tetera" y le aplico el material
+    glPushMatrix();
+//setMaterial
+    glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, shine);
+    glutSolidSphere(80.0, 50,50);
+    glFlush();
 }
 
-// Función para controlar teclas especiales
-void specialKeys( int key, int x, int y )
-{
-
-    //  Flecha derecha: aumentar rotación 7 grados
-    if (key == GLUT_KEY_RIGHT)
-        rotate_y += 7;
-
-    //  Flecha izquierda: rotación en eje Y negativo 7 grados
-    else if (key == GLUT_KEY_LEFT)
-        rotate_y -= 7;
-    //  Flecha arriba: rotación en eje X positivo 7 grados
-    else if (key == GLUT_KEY_UP)
-        rotate_x += 7;
-    //  Flecha abajo: rotación en eje X negativo 7 grados
-    else if (key == GLUT_KEY_DOWN)
-        rotate_x -= 7;
-    //  Tecla especial F2 : rotación en eje Z positivo 7 grados
-    else if (key == GLUT_KEY_F2)
-        rotate_z += 7;
-    //  Tecla especial F2 : rotación en eje Z negativo 7 grados
-    else if (key == GLUT_KEY_F2)
-        rotate_z -= 7;
-
-    //  Solicitar actualización de visualización
-    glutPostRedisplay();
-
-}
-
-// Función para controlar teclas normales del teclado.
+// Termina la ejecucion del programa cuando se presiona ESC
 void keyboard(unsigned char key, int x, int y)
 {
-    //control de teclas que hacen referencia a Escalar y transladar el cubo en los ejes X,Y,Z.
     switch (key)
     {
-    case 's':
-        scale=0.5;
+    case 27:
+        exit(0);
         break;
-    case 'd':
-        scale=0.5;
-        break;
-    case 'x' :
-        X += 0.1f;
-        break;
-    case 'X' :
-        X -= 0.1f;
-        break;
-    case 'y' :
-        Y += 0.1f;
-        break;
-    case 'Y' :
-        Y -= 0.1f;
-        break;
-    case 'z':
-        Z -= 0.1f;
-        break;
-    case 'Z':
-        Z += 0.1f;
-        break;
-    case 'q':
-        exit(0);			// exit
     }
-    glutPostRedisplay();
 }
 
-
-int main(int argc, char** argv)
+// Main del programa.
+int main(int argc, char **argv)
 {
-// Inicializa la librería auxiliar GLUT
+// Inicializo OpenGL
     glutInit(&argc, argv);
-// Inicializa el modo de visualización
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-// Indica el tamaño de la ventana (ancho,alto)
-    glutInitWindowSize(500, 500);
-// Indica la posición inicial (xmin,ymin)
-    glutInitWindowPosition(100, 100);
-// Abre la ventana con el título indicado
-    glutCreateWindow("Dibujando una tetera 3d");
-// Inicializar valores
+
+// Activamos buffer simple y colores del tipo RGB
+    glutInitDisplayMode (GLUT_RGB | GLUT_DEPTH);
+
+// Definimos una ventana de medidas 800 x 600 como ventana
+// de visualizacion en pixels
+    glutInitWindowSize (800, 600);
+
+// Posicionamos la ventana en la esquina superior izquierda de
+// la pantalla.
+    glutInitWindowPosition (0, 0);
+
+// Creamos literalmente la ventana y le adjudicamos el nombre que se
+// observara en su barra de titulo.
+    glutCreateWindow ("Teapot");
+
+// Inicializamos el sistema
     init();
-// Indica cual es la función de dibujo
+
     glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
     glutKeyboardFunc(keyboard);
-    glutSpecialFunc(specialKeys);
-// Indica cual es la función para el cambio de tamaño de la ventana
-    //glutReshapeFunc(reshape);
-// Comienza el bucle de dibujo y proceso de eventos.
     glutMainLoop();
+
+    return 0;
 }
